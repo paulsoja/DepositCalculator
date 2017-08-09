901334e8c284be2aebec32ea99e25a76f130d6bc
PavelSoja <sojasoft2015@gmail.com>
2017-08-09 22:03:17 +0300

    added toolbar

--- app/src/main/java/com/paulsoja/depositcalculator/Activity/MainActivity.java
+++ app/src/main/java/com/paulsoja/depositcalculator/Activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.paulsoja.depositcalculator.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.paulsoja.depositcalculator.Fragments.InputDepositDataFragment;
 import com.paulsoja.depositcalculator.R;
@@ -12,7 +13,11 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(toolbar);
+
         InputDepositDataFragment inputFragment = new InputDepositDataFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, inputFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, inputFragment).commit();
     }
 }
